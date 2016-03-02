3c635dc75d3d3fee0f4c286c41a74000ca0ace71
robmyers82 <robert_myers_@hotmail.com>
2016-03-02 11:05:41 -0400

    Adding file upload to mobile app.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -38,7 +38,7 @@ angular.module('starter.controllers', ['ngStorage'])
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    $http.post("http://localhost:8080/api/login", { email: $scope.loginData.username, password: $scope.loginData.password }).then(function(result) {
+    $http.post("http://104.131.76.58:8080/api/login", { email: $scope.loginData.username, password: $scope.loginData.password }).then(function(result) {
         if (result.data.loginstatus == "success") {
             // successful login, in our example, we will just send an alert message
             $localStorage.user_id = result.data.userid;
@@ -53,19 +53,21 @@ angular.module('starter.controllers', ['ngStorage'])
         }
     }, function(error) {
         alert("There was a problem getting your profile.  Check the logs for details.");
-        console.log(error);
+        alert(JSON.stringify(error));
     });
   };
 })
 
-.controller('TodoCtrl', function($state, $scope, $http, $ionicLoading, $localStorage) {
+.controller('TodoCtrl', function($state, $scope, $http, $ionicLoading, $localStorage, $cordovaFileTransfer) {
   $scope.todos = [];
+  $scope.data = {};
+  $scope.data.imageURI = "";
 
   $ionicLoading.show({
       template: 'Loading Todos...'
   });
   
-  $http.get("http://localhost:8080/api/todos").then(function(result) {
+  $http.get("http://104.131.76.58:8080/api/todos").then(function(result) {
       $scope.todos = result.data;
       $ionicLoading.hide();
   }, function(error) {
@@ -80,20 +82,60 @@ angular.module('starter.controllers', ['ngStorage'])
       template: 'Adding your Todo...'
     });
 
-    $http.post("http://localhost:8080/api/phonetodos", {
-      token: $localStorage.token,
-      user_id: $localStorage.user_id,
-      info: newtodo
-    }).then(function(result) {
-        $scope.todos = result.data;
-        $state.go('app.todos');
-        $ionicLoading.hide();
-    }, function(error) {
-        alert("There was a problem getting your profile.  Check the logs for details.");
-        console.log(error);
-        $ionicLoading.hide();
-        $state.go('app.todos');
+    var filename = "photo.png";
+
+    var options = {
+      fileKey: "file",
+      fileName: filename,
+      chunkedMode: false,
+      mimeType: "image/png",
+      params: {
+        'token': $localStorage.token,
+        'user_id': $localStorage.user_id,
+        'info': newtodo
+      }
+    };
+
+    $cordovaFileTransfer.upload("http://104.131.76.58:8080/api/phonetodos", $scope.data.imageURI, options).then(function(result) {
+      // Success!
+      alert("Success!");
+      $scope.todos = result.data;
+      $state.go('app.todos');
+      $ionicLoading.hide();
+    }, function(err) {
+      // Error
+      alert("There was a problem getting your profile.  Check the logs for details.");
+    }, function (progress) {
+      // constant progress updates
     });
+  }
+
+  // the takePhoto function attached to the button
+  $scope.takePhoto = function() {
+
+    // default camera options, please see https://github.com/apache/cordova-plugin-camera/blob/master/doc/index.md
+    // for the complete list
+    var cameraOptions = {
+      destinationType: Camera.DestinationType.FILE_URI,
+      targetWidth: 900,
+      targetHeight: 900
+    };
+
+    // perform the api call to take the picture.  
+    // The success function has a URI containing the file location on the phone of the image
+    // The error function sends an alert that an issue has occured
+    // The cameraOptions are defined above
+    navigator.camera.getPicture(function(data) {
+      
+      // We need to encode the returned data in this way so that it can be displayed on the phone as raw data
+      // instead of a URL
+      $scope.data.imageURI = data;
+      $state.go('app.todoadd');
+
+    }, function(err) {
+
+      alert("Oops!  Can't take your photo!  Either you backed out before saving a photo, or you are not on a device.  Camera will not work from the emulator...");
+    }, cameraOptions);
   }
 
   $scope.deleteTodo = function(id) {
@@ -101,7 +143,7 @@ angular.module('starter.controllers', ['ngStorage'])
         template: 'Removing your Todo...'
       });
 
-      $http.delete('http://localhost:8080/api/todos/' + id).success(function(data) {
+      $http.delete('http://104.131.76.58:8080/api/todos/' + id).success(function(data) {
           $scope.todos = data; // assign our new list of todos
           $state.go($state.current, {}, {reload: true});
           $ionicLoading.hide();
